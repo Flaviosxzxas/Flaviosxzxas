91dfe0fba5434d117c529589f60e8f06bce9248f
Flaviosxzxas <132931370+Flaviosxzxas@users.noreply.github.com>
2025-01-03 18:55:29 -0300

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -524,5 +524,10 @@ if [ -f /var/run/reboot-required ]; then
   sleep 5
   sudo reboot
 else
-  echo "Reboot não necessário. Finalizando o script."
+  echo "Reboot não necessário. Aguardando 5 segundos para leitura antes de finalizar o script..."
+  sleep 5
 fi
+
+# Finaliza o script explicitamente
+echo "Finalizando o script."
+exit 0
